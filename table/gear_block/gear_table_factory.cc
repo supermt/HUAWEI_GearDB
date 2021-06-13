6ffcfa40706e3a8937f2e2601359eeb91dfceeda
supermt <supertempler@gmail.com>
2021-06-13 23:06:02 +0800

    Add the skeleton functions for the gear table. Warning! There are tons of bugs here.

--- table/gear_block/gear_table_factory.cc
+++ table/gear_block/gear_table_factory.cc
@@ -2,4 +2,80 @@
 // Created by jinghuan on 6/12/21.
 //
 
-#include "gear_table_factory.h"
+#ifndef ROCKSDB_LITE
+#include "table/gear_block/gear_table_factory.h"
+
+#include <stdint.h>
+
+#include <memory>
+
+#include "db/dbformat.h"
+#include "options/options_helper.h"
+#include "port/port.h"
+#include "rocksdb/convenience.h"
+#include "table/gear_block/gear_table_builder.h"
+#include "table/gear_block/gear_table_reader.h"
+#include "util/string_util.h"
+
+namespace ROCKSDB_NAMESPACE {
+static std::unordered_map<std::string, OptionTypeInfo> plain_table_type_info = {
+    {"user_key_len",
+     {offsetof(struct GearTableOptions, user_key_len), OptionType::kUInt32T,
+      OptionVerificationType::kNormal, OptionTypeFlags::kNone, 0}},
+    {"user_value_len",
+     {offsetof(struct GearTableOptions, user_key_len), OptionType::kUInt32T,
+      OptionVerificationType::kNormal, OptionTypeFlags::kNone, 0}},
+    {"encoding_type",
+     {offsetof(struct GearTableOptions, encoding_type),
+      OptionType::kEncodingType, OptionVerificationType::kByName,
+      OptionTypeFlags::kNone, 0}},
+};
+
+Status GearTableFactory::NewTableReader(
+    const TableReaderOptions &table_reader_options,
+    std::unique_ptr<RandomAccessFileReader> &&file, uint64_t file_size,
+    std::unique_ptr<TableReader> *table,
+    bool /*prefetch_index_and_filter_in_cache*/) const {
+  // Create the external index file when the file.
+
+  return GearTableReader::Open(table_reader_options.ioptions,
+                               table_reader_options.env_options,
+                               table_reader_options.internal_comparator,
+                               std::move(file), file_size, table);
+  //  return Status::OK();
+  return GearTableReader::Open(
+      table_reader_options.ioptions, table_reader_options.env_options,
+      table_reader_options.internal_comparator, std::move(file), file_size,
+      table, table_reader_options.immortal,
+      table_reader_options.prefix_extractor);
+}
+
+TableBuilder *GearTableFactory::NewTableBuilder(
+    const TableBuilderOptions &table_builder_options, uint32_t column_family_id,
+    rocksdb::WritableFileWriter *file) const {
+  return nullptr;
+}
+
+std::string GearTableFactory::GetPrintableTableOptions() const {
+  std::string ret;
+  ret.reserve(20000);
+  const int kBufferSize = 200;
+  char buffer[kBufferSize];
+
+  snprintf(buffer, kBufferSize, "  user_key_len: %u\n",
+           table_options_.user_key_len);
+  ret.append(buffer);
+  snprintf(buffer, kBufferSize, "  user_value_len: %u\n",
+           table_options_.user_value_len);
+  ret.append(buffer);
+  snprintf(buffer, kBufferSize, "  encoding_type: %d\n",
+           table_options_.encoding_type);
+  ret.append(buffer);
+  return ret;
+}
+
+const GearTableOptions &GearTableFactory::table_options() const {
+  return table_options_;
+}
+}  // namespace ROCKSDB_NAMESPACE
+#endif  // ROCKSDB_LITE
--- table/gear_block/gear_table_factory.h
+++ table/gear_block/gear_table_factory.h
@@ -1,10 +1,71 @@
-//
-// Created by jinghuan on 6/12/21.
-//
+// Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
 
-#ifndef ROCKSDB_GEAR_TABLE_FACTORY_H
-#define ROCKSDB_GEAR_TABLE_FACTORY_H
+#pragma once
 
-class gear_table_factory {};
+#ifndef ROCKSDB_LITE
+#include <memory>
+#include <string>
+#include <stdint.h>
 
-#endif  // ROCKSDB_GEAR_TABLE_FACTORY_H
+#include "options/options_helper.h"
+#include "rocksdb/options.h"
+#include "rocksdb/table.h"
+
+namespace ROCKSDB_NAMESPACE {
+
+struct EnvOptions;
+
+class Status;
+class RandomAccessFile;
+class WritableFile;
+class Table;
+class TableBuilder;
+
+class GearTableFactory : public TableFactory {
+ public:
+  ~GearTableFactory() {}
+  explicit GearTableFactory(
+      const GearTableOptions& _table_options = GearTableOptions())
+      : table_options_(_table_options) {}
+
+  const char* Name() const override { return "GearTable"; }
+  Status NewTableReader(
+      const TableReaderOptions& table_reader_options,
+      std::unique_ptr<RandomAccessFileReader>&& file, uint64_t file_size,
+      std::unique_ptr<TableReader>* table_reader,
+      bool prefetch_index_and_filter_in_cache = true) const override;
+
+  TableBuilder* NewTableBuilder(
+      const TableBuilderOptions& table_builder_options,
+      uint32_t column_family_id, WritableFileWriter* file) const override;
+
+  std::string GetPrintableTableOptions() const override;
+
+  const GearTableOptions& table_options() const;
+
+  static const char kValueTypeSeqId0 = char(~0);
+
+  // Sanitizes the specified DB Options.
+  Status SanitizeOptions(
+      const DBOptions& /*db_opts*/,
+      const ColumnFamilyOptions& /*cf_opts*/) const override {
+    return Status::OK();
+  }
+
+  void* GetOptions() override { return &table_options_; }
+
+  Status GetOptionString(const ConfigOptions& /*config_options*/,
+                         std::string* /*opt_string*/) const override {
+    return Status::OK();
+  }
+
+ private:
+  GearTableOptions table_options_;
+};
+
+
+}  // namespace ROCKSDB_NAMESPACE
+#endif  // ROCKSDB_LITE
